4ec9b719682e31070cd101115e42908520223066
Cristian Eduardo Selinger <selingercristian@outlook.com>
2022-04-24 21:22:03 -0300

    Add actions pause game menu

--- menu/PauseMenu.gd
+++ menu/PauseMenu.gd
@@ -5,4 +5,13 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
-		visible = new_pause_state 
+		visible = new_pause_state
+		get_node("Controls/ReturnGame").grab_focus()
+
+func _on_ReturnGame_pressed():
+	get_tree().paused = false
+	visible = false
+
+func _on_MainMenu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://menu/Menu.tscn")
